6ee470f3186bcdd9c42654b59a63398cb9287ac1
Stepan <pulin2@mail.ru>
2023-10-22 10:50:19 +0500

    added octaves in NoiseMap and added persustabce and lacunarity

--- GameProject/Assets/Scripts/ProceduralGenerate/Noise.cs
+++ GameProject/Assets/Scripts/ProceduralGenerate/Noise.cs
@@ -9,9 +9,12 @@ namespace TheIslandKOD
         /// </summary>
         /// <param name="mapWidth">Noise map Width</param>
         /// <param name="mapHeight">Noise map Height</param>
-        /// <param name="scale"></param>
+        /// <param name="scale">scale noise map</param>
+        /// <param name="octaves">count perlin layers</param>
+        /// <param name="persistance">Height noise map</param>
+        /// <param name="lacunarity">coordinate scale</param>
         /// <returns>noiseMap type float[,]</returns>
-        public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
+        public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, float persistance, float lacunarity)
         {
             float[,] noiseMap = new float[mapWidth, mapHeight];
 
@@ -21,17 +24,50 @@ namespace TheIslandKOD
                 scale = 0.001f;
             }
 
+            float maxNoiseHeight = float.MinValue;
+            float minNoiseHeight = float.MaxValue;
+
             //Initialize noiseMap
             for (int y = 0; y < mapHeight; y++)
             {
                 for (int x = 0; x < mapWidth; x++)
                 {
-                    float sampleX = x / scale;
-                    float sampleY = y / scale;
+                    float amplitude = 1;
+                    float frequancy = 1;
+                    float noiseHeight = 0;
 
-                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                    for (int i = 0; i < octaves; i++)
+                    {
 
-                    noiseMap[x, y] = perlinValue;
+                        float sampleX = x / scale * frequancy;
+                        float sampleY = y / scale * frequancy;
+
+                        float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                        noiseHeight += perlinValue * amplitude;
+
+                        amplitude *= persistance;
+                        frequancy *= lacunarity;
+                        
+                    }
+
+                    if (maxNoiseHeight < noiseHeight)
+                    {
+                        maxNoiseHeight = noiseHeight;
+                    }
+                    else if (minNoiseHeight > noiseHeight)
+                    {
+                        minNoiseHeight = noiseHeight;
+                    }
+
+                    noiseMap[x, y] = noiseHeight;
+                }
+            }
+
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for (int x = 0; x < mapWidth; x++)
+                {
+                    noiseMap[y, x] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[y, x]);
                 }
             }
 
