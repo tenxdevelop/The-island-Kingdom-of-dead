34ae01b601e7456f4ac65a423fa418624d734a06
Stepan <pulin2@mail.ru>
2023-10-26 00:43:56 +0500

    realized drag and drop handler for invetory

---
+++ GameProject/Assets/Scripts/Inventory/UISlot.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UISlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        var otherItemTransform = eventData.pointerDrag.transform;
+        otherItemTransform.SetParent(transform);
+        otherItemTransform.localPosition = Vector3.zero;
+    }
+}
